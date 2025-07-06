512490a55bae10e5391d4100609330d5c067e8ac
Stas Korniev <Stask4100@gmail.com>
2025-07-06 09:24:47 +0300

    додано веб-перегляд усіх користувачів

---
+++ src/main/java/com/korniev/stas/user_management_system/controller/ViewController.java
@@ -0,0 +1,27 @@
+package com.korniev.stas.user_management_system.controller;
+
+import com.korniev.stas.user_management_system.model.User;
+import com.korniev.stas.user_management_system.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class ViewController {
+
+    private final UserService userService;
+
+    public ViewController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users/view")
+    public String viewUsers(Model model) {
+        List<User> users = userService.getAllUsers();
+        model.addAttribute("users", users);
+        return "user-list";
+    }
+
+}
